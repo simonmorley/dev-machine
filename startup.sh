53a058b4fddda272a33b501784e7ba1dcd3fb747
Simon Morley <>
2019-04-03 22:54:17 +0100

    first commit

--- startup.sh
+++ startup.sh
@@ -7,4 +7,5 @@ gcloud config set project $PROJECT
 gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
+
 echo "done"
